34df5f2bd89347b826b3ea0453a3cad80dd087ad
Evgeniy Lobanov <evglbnv@yandex.ru>
2022-08-23 19:02:06 +0300

    Начало 5 пр

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,83 @@
-const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.profile__edit');
-const closeButton = document.querySelector('.popup__close');
+const popup = document.querySelector(".popup");
+const popupEdit = document.querySelector(".popup_type_edit-profile");
+const popupAdd = document.querySelector(".popup_type_add-card");
 
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const jobInput = formElement.querySelector('.popup__input_type_job');
-const profileSubmit = formElement.querySelector('popup__button')
+const openButton = document.querySelector(".profile__edit");
+const openButtonAdd = document.querySelector(".profile__add");
+const closeButton = document.querySelector(".popup__close");
+const closeButtonAdd = document.querySelector("#popupAddCloseBtn");
 
-const profileElement = document.querySelector('.profile');
-const profileName = profileElement.querySelector('.profile__name');
-const profileJob = profileElement.querySelector('.profile__profession');
+const formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const jobInput = formElement.querySelector(".popup__input_type_job");
+const profileSubmit = formElement.querySelector("popup__button");
+
+const profileElement = document.querySelector(".profile");
+const profileName = profileElement.querySelector(".profile__name");
+const profileJob = profileElement.querySelector(".profile__profession");
+
+// Попап открытия формы
+// Открыть Popup редактирования формы
 
 function openPopup() {
-    popup.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+  popupEdit.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
+
+// Закрыть Popup редактирования формы
 
 function closePopup() {
-    popup.classList.remove('popup_opened');
+  popupEdit.classList.remove("popup_opened");
 }
 
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+openButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+
+//сабмит формы редактирования профиля
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    closePopup(profileSubmit);
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(profileSubmit);
 }
 
+formElement.addEventListener("submit", formSubmitHandler);
 
-formElement.addEventListener('submit', formSubmitHandler);
+// Попап добавления карточки формы
+
+function openPopupAdd() {
+  popupAdd.classList.add("popup_opened");
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove("popup_opened");
+}
+
+openButtonAdd.addEventListener("click", openPopupAdd);
+closeButtonAdd.addEventListener("click", closePopupAdd);
+
+//Добавление 6 карточек из коробки
+
+const cardTemplate = document.querySelector(".cards__template");
+const listElement = document.querySelector(".element__list");
+const inputElementAddName = document.querySelector(".popup__input_place_name");
+const inputElementAddLink = document.querySelector(".popup__input_place_link");
+const formAddCard = document.querySelector("#popupAddForm");
+const formAddPlaceName = formAddCard.querySelector(".popup__input_place_name");
+const formAddLink = formAddCard.querySelector(".popup__input_place_link");
+const addCardSubmitBtn = document.querySelector("#popupAddSubmitBtn");
+
+function addCard(cardName, cardLink) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+  cardElement.querySelector(".element__text").textContent = cardName;
+  cardElement.querySelector(".element__image").src = cardLink;
+
+  listElement.append(addCard);
+}
+
+initialCards.forEach((element) => {
+  addCard(element.name, element.link);
+});
